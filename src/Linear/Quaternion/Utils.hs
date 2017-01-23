d19f469c02155f67e78bf703e80bc5583551b071
Andy Gill <andygill@ku.edu>
2017-01-23 14:42:02 -0600

    Add rotateBetween

--- src/Linear/Quaternion/Utils.hs
+++ src/Linear/Quaternion/Utils.hs
@@ -1,11 +1,13 @@
 module Linear.Quaternion.Utils where
 
-import Linear.V3 (V3(..),cross)
-import Linear.Vector
 import Linear.Affine
+import Linear.Epsilon
+import Linear.Matrix as M
 import Linear.Metric
 import Linear.Quaternion
-import Linear.Matrix as M
+import Linear.V3 (V3(..),cross)
+import Linear.Vector
+
 
 -- | This is an alternative to the Quaternion format,
 --   following the THREE.js representation of Euler.
@@ -21,10 +23,11 @@ data Euler a = Euler
 data Order = XYZ | XZY | YXZ | YZX | ZXY | ZYX
      deriving (Eq, Ord, Show, Read)
 
--- adapted from https://github.com/mrdoob/three.js/blob/master/src/math/Euler.js
+-- | Convert a 'Quaternion' into a (ordered) 'Euler'.
 quaternionToEuler :: RealFloat a => Order -> Quaternion a -> Euler a
 quaternionToEuler o@YXZ q = Euler o rX rY rZ
   where
+     -- adapted from https://github.com/mrdoob/three.js/blob/master/src/math/Euler.js
      V3 (V3 m11 m12 m13)
         (V3 m21 m22 m23)
         (V3 m31 m32 m33) = id $ fromQuaternion q
@@ -35,3 +38,21 @@ quaternionToEuler o@YXZ q = Euler o rX rY rZ
      rZ | abs m23 < 0.99999 = atan2 m21    m22
         | otherwise         = 0
                
+
+-- | Take two vectors, and figure out how to rotate between them.
+rotateBetween :: (RealFloat a, Show a, Epsilon a, Floating a) => V3 a -> V3 a -> Quaternion a
+rotateBetween v1 v2 
+    | nearZero c && nearZero (d - 1) = 1
+    | nearZero c && nearZero (d + 1) && (nearZero (mid - v1) || nearZero (mid - v2)) 
+                                     = axisAngle (V3 1 0 0) pi 
+    | nearZero c && nearZero (d + 1) = rotateBetween v1 mid * rotateBetween mid v2
+    | otherwise  = axisAngle cn ca
+  where
+    c@(V3 xc yc zc) = cross v1 v2
+    d = dot (normalize v1) (normalize v2)
+    cn = normalize c
+    ca = acos d
+    q = axisAngle cn ca
+    
+    mid = V3 0 1 0
+        
